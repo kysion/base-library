3ebd9d6c5acb2fbcfe1184c3cf475c6dfb4b9373
0852FeiFeiLin <2778368047@qq.com>
2023-04-18 22:38:39 +0800

    pref: 优化funs包命名

--- utility/daoctl/index.go
+++ utility/daoctl/index.go
@@ -1,87 +1,87 @@
 package daoctl
 
 import (
-    "github.com/gogf/gf/v2/database/gdb"
-    "github.com/gogf/gf/v2/util/gconv"
-    "github.com/kysion/base-library/base_model"
-    "github.com/kysion/base-library/utility/daoctl/internal"
-    "math"
+	"github.com/gogf/gf/v2/database/gdb"
+	"github.com/gogf/gf/v2/util/gconv"
+	"github.com/kysion/base-library/base_model"
+	"github.com/kysion/base-library/utility/daoctl/internal"
+	"math"
 )
 
 func GetById[T any](model *gdb.Model, id int64) *T {
-    return Scan[T](model.Where("id", id))
+	return Scan[T](model.Where("id", id))
 }
 
 func GetByIdWithError[T any](model *gdb.Model, id int64) (*T, error) {
-    return ScanWithError[T](model.Where("id", id))
+	return ScanWithError[T](model.Where("id", id))
 }
 
 func Find[T any](db *gdb.Model, orderBy []base_model.OrderBy, searchFields ...base_model.FilterInfo) (response *base_model.CollectRes[T], err error) {
-    return Query[T](db, &base_model.SearchParams{
-        Filter: searchFields,
-        Pagination: base_model.Pagination{
-            PageNum:  1,
-            PageSize: -1,
-        },
-        OrderBy: orderBy,
-    }, true)
+	return Query[T](db, &base_model.SearchParams{
+		Filter: searchFields,
+		Pagination: base_model.Pagination{
+			PageNum:  1,
+			PageSize: -1,
+		},
+		OrderBy: orderBy,
+	}, true)
 }
 
 func GetAll[T any](db *gdb.Model, info *base_model.Pagination) (response *base_model.CollectRes[*T], err error) {
-    total, err := db.Count()
-    entities := make([]*T, 0, total)
-    if info == nil {
-        info = &base_model.Pagination{
-            PageNum:  1,
-            PageSize: gconv.Int(total),
-        }
-    }
+	total, err := db.Count()
+	entities := make([]*T, 0, total)
+	if info == nil {
+		info = &base_model.Pagination{
+			PageNum:  1,
+			PageSize: gconv.Int(total),
+		}
+	}
 
-    if err != nil {
-        return
-    }
-    err = db.Page(info.PageNum, info.PageSize).Scan(&entities)
+	if err != nil {
+		return
+	}
+	err = db.Page(info.PageNum, info.PageSize).Scan(&entities)
 
-    return &base_model.CollectRes[*T]{
-        Records: entities,
-        PaginationRes: base_model.PaginationRes{
-            Pagination: *info,
-            PageTotal:  gconv.Int(math.Ceil(gconv.Float64(total) / gconv.Float64(info.PageSize))),
-            Total:      gconv.Int64(total),
-        },
-    }, nil
+	return &base_model.CollectRes[*T]{
+		Records: entities,
+		PaginationRes: base_model.PaginationRes{
+			Pagination: *info,
+			PageTotal:  gconv.Int(math.Ceil(gconv.Float64(total) / gconv.Float64(info.PageSize))),
+			Total:      gconv.Int64(total),
+		},
+	}, nil
 }
 
 // Query IsExport参数为true不进行分页导出，反之会进行分页导出
 func Query[T any](db *gdb.Model, searchFields *base_model.SearchParams, IsExport bool) (response *base_model.CollectRes[T], err error) {
-    if searchFields == nil {
-        searchFields = &base_model.SearchParams{}
-    }
+	if searchFields == nil {
+		searchFields = &base_model.SearchParams{}
+	}
 
-    // 查询具体的值
-    queryDb, _ := internal.MakeBuilder(db, searchFields.Filter)
-    queryDb = internal.MakeOrderBy(queryDb, searchFields.OrderBy)
+	// 查询具体的值
+	queryDb, _ := internal.MakeBuilder(db, searchFields.Filter)
+	queryDb = internal.MakeOrderBy(queryDb, searchFields.OrderBy)
 
-	if searchFields.PageNum < 1 {
+	if searchFields.PageNum <= 1 {
 		searchFields.PageNum = 1
 	}
 
-    if searchFields.PageSize == 0 {
-        searchFields.PageSize = 20
-    }
+	if searchFields.PageSize <= 0 {
+		searchFields.PageSize = 20
+	}
 
-    entities := make([]T, 0)
-    if searchFields == nil || IsExport {
-        searchFields.PageSize = -1
-        err = queryDb.Scan(&entities)
-    } else {
-        err = queryDb.Page(searchFields.PageNum, searchFields.PageSize).Scan(&entities)
-    }
+	entities := make([]T, 0)
+	if searchFields == nil || IsExport {
+		searchFields.PageSize = -1
+		err = queryDb.Scan(&entities)
+	} else {
+		err = queryDb.Page(searchFields.PageNum, searchFields.PageSize).Scan(&entities)
+	}
 
-    response = &base_model.CollectRes[T]{
-        Records:       entities,
-        PaginationRes: internal.MakePaginationArr(db, searchFields.Pagination, searchFields.Filter),
-    }
+	response = &base_model.CollectRes[T]{
+		Records:       entities,
+		PaginationRes: internal.MakePaginationArr(db, searchFields.Pagination, searchFields.Filter),
+	}
 
-    return response, nil
+	return response, nil
 }
