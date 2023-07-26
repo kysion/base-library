b0f6564fb0ebf6050578ce9ea3b276c1047783f5
Arise <2778368047@qq.com>
2023-07-26 18:53:14 +0800

    feat: 将验证码 枚举抽取到base库

---
+++ base_model/base_enum/base_enum.go
@@ -0,0 +1,13 @@
+package base_enum
+
+import (
+	sys_enum_captcha "github.com/kysion/base-library/base_model/base_enum/internal/captcha"
+)
+
+type (
+	CaptchaType sys_enum_captcha.CaptchaTypeEnum
+)
+
+var (
+	Captcha = sys_enum_captcha.Captcha
+)
