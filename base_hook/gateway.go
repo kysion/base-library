6c392d9187c8085c1cc9a93f116482334937aa1f
Arise <2778368047@qq.com>
2024-08-03 12:08:53 +0800

    feat

--- base_hook/gateway.go
+++ base_hook/gateway.go
@@ -2,10 +2,10 @@ package base_hook
 
 import (
 	"context"
-	"fmt"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/kysion/base-library/base_model"
 	"github.com/kysion/base-library/base_model/base_enum"
+	"reflect"
 )
 
 // GatewayHook 网关Hook
@@ -56,30 +56,55 @@ func (s *sGateway) BroadcastMessage(model base_model.HookModel) {
 func RegisterHookMessage[K any, F any](hook *BaseHook[K, F]) {
 	gateway.RegisterHookMessage(func(model base_model.HookModel) {
 		if model.BusinessType().Code() == hook.GetBusinessType().Code() {
-			PublishHookMessage(context.Background(), hook, Option{})
+			PublishHookMessage(context.Background(), hook, Option{Data: model.Data})
 		}
 	})
 }
 
 // PublishHookMessage 发布Hook消息
-func PublishHookMessage[K any, F any](ctx context.Context, hook *BaseHook[K, F], option Option, f ...func(ctx context.Context, data any, v any) error) {
+func PublishHookMessage[K any, F any](ctx context.Context, hook *BaseHook[K, F], option Option) []error {
+	var errArr []error
 	hook.Iterator(func(key K, value F) {
-		_ = g.Try(ctx, func(ctx context.Context) {
-			var err error
-			if len(f) > 0 {
-				err = f[0](ctx, option.Data, value)
-				if err != nil {
-					fmt.Println(err)
+		var err error
+		err = g.Try(ctx, func(ctx context.Context) {
+			of := reflect.ValueOf(value)
+			retArr := of.Call([]reflect.Value{reflect.ValueOf(ctx), reflect.ValueOf(option.Data)})
+			if len(retArr) > 0 {
+				ret := retArr[0].Interface()
+				if ret != nil {
+					err = ret.(error)
 				}
-				return
-			} else {
-				var vFunc interface{} = value
-				err = vFunc.(CommonHookFunc)(ctx, option.Data)
-			}
-
-			if err != nil {
-				fmt.Println(err)
 			}
 		})
+		if err != nil {
+			g.Log().Error(ctx, err)
+			errArr = append(errArr, err)
+		}
 	}, option)
+	return errArr
 }
+
+//
+//
+//// PublishHookMessage 发布Hook消息
+//func PublishHookMessage[K any, F any](ctx context.Context, hook *BaseHook[K, F], option Option, f ...func(ctx context.Context, data any, v any) error) {
+//	hook.Iterator(func(key K, value F) {
+//		_ = g.Try(ctx, func(ctx context.Context) {
+//			var err error
+//			if len(f) > 0 {
+//				err = f[0](ctx, option.Data, value)
+//				if err != nil {
+//					fmt.Println(err)
+//				}
+//				return
+//			} else {
+//				var vFunc interface{} = value
+//				err = vFunc.(CommonHookFunc)(ctx, option.Data)
+//			}
+//
+//			if err != nil {
+//				fmt.Println(err)
+//			}
+//		})
+//	}, option)
+//}
