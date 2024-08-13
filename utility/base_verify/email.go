a6d7e2df8d13f01413360d57d936d70febc3bdd0
Arise <2778368047@qq.com>
2024-08-13 14:50:32 +0800

    fix: 修复邮箱IsEmail 方法不能校验 点 “ . ”  的Bug

--- utility/base_verify/email.go
+++ utility/base_verify/email.go
@@ -1,12 +1,26 @@
 package base_verify
 
 import (
-	"regexp"
+	"github.com/gogf/gf/v2/text/gregex"
 )
 
 // IsEmail 识别电子邮箱
 func IsEmail(email string) bool {
-	result, _ := regexp.MatchString(`^([\w\.\_\-]{2,10})@(\w{1,}).([a-z]{2,4})$`, email)
+	// 不能匹配 .  supen.huang@qq.com
+	//result, _ := regexp.Compile(`^([\w\.\_\-]{2,10})@(\w{1,}).([a-z]{2,4})$`)
+
+	result := gregex.IsMatchString(
+		`^[a-zA-Z0-9_\-\.]+@[a-zA-Z0-9_\-]+(\.[a-zA-Z0-9_\-]+)+$`,
+		email,
+	)
+
+	// 正则匹配邮箱
+	//emailRegex, _ := regexp.Compile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	//if emailRegex.Match([]byte(email)) {
+	//	return true
+	//} else {
+	//	return false
+	//}
 
 	return result
 }
